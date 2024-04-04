702a345db46d77cba0914a8ca0913e35c5253aa1
ducvu <ducvu973xd@gmail.com>
2024-04-04 20:59:14 +0700

    setting up

--- main.py
+++ main.py
@@ -5,6 +5,18 @@ pygame.init()
 screen = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("Space Invaders")
 
+# player
+class Player():
+  def __init__(self):
+    self.image = pygame.image.load("home/kira/workspace/projects/space_invaders/assets/player.png").convert_alpha()
+    self.rect = self.image.get_rect()
+    self.speed = 5
+
+# enemies
+class Enemy:
+  def __init__(self):
+    pass
+
 while True:
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
